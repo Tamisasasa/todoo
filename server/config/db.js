10d7afaa5ffbab01a2fad0f6a59d0a2ab477ea2c
Ijo <infoitsci@mju.ac.th>
2026-08-31 02:36:54 +0700

    แก้ไข้อย

--- server/config/db.js
+++ server/config/db.js
@@ -1,17 +1,19 @@
 const mysql = require('mysql2')
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'todoo',
-    dateStrings: true  
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
+    dateStrings: true
 })
 
 db.connect((err) => {
     if (err) {
         return console.log('MySQL connection failed:', err)
     }
+    console.log('MySQL connected')
 })
 
 module.exports = db
