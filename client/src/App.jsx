06b81b3e18158c6d89cacadaf04bf5160b33f6c6
Ijo <infoitsci@mju.ac.th>
2026-08-31 03:06:14 +0700

    แก้ไข้อย

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Daily from './pages/Daily'
 import Weekly from './pages/Weekly'
 
 function App() {
   return (
-    <BrowserRouter basename="/todoo">
-      <Routes>
-        <Route path="/" element={<Weekly />} />
-        <Route path="/daily" element={<Daily />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Weekly />} />
+      <Route path="/daily" element={<Daily />} />
+    </Routes>
   )
 }
 
